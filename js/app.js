4caa9994080eaf7c94daa61cdd60c7807c95d8f1
Vaibhav <vaibhavmor@Vaibhavs-iMac.local>
2018-07-13 22:14:13 +1200

    Add code to disable send btn on load

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 // Variables
-
+const sendBtn = document.getElementById('sendBtn');
 
 // Event Listeners
 
+eventListeners();
+
+function eventListeners() {
+    // App Init
+    document.addEventListener('DOMContentLoaded', appInit);
+}
+
 
 // Functions
+
+// App Initilisation
+function appInit() {
+    // disable the send button on load
+    sendBtn.disabled = true;
+}
