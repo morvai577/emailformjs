2e6f5d6b3d200e6082ed4fa50fc893c53f552aa0
Vaibhav <vaibhavmor@Vaibhavs-iMac.local>
2018-07-13 22:29:05 +1200

    Add code to validate fieldsusing UI elements

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 // Variables
-const sendBtn = document.getElementById('sendBtn');
+const sendBtn = document.getElementById('sendBtn'),
+        email = document.getElementById('email'),
+        subject = document.getElementById('subject'),
+        message = document.getElementById('message');
 
 // Event Listeners
 
@@ -8,6 +11,11 @@ eventListeners();
 function eventListeners() {
     // App Init
     document.addEventListener('DOMContentLoaded', appInit);
+
+    // Validate the form
+    email.addEventListener('blur', validateField);
+    subject.addEventListener('blur', validateField);
+    message.addEventListener('blur', validateField);
 }
 
 
@@ -17,4 +25,41 @@ function eventListeners() {
 function appInit() {
     // disable the send button on load
     sendBtn.disabled = true;
+}
+
+// Validate the fields
+function validateField() {
+    let errors;
+
+    // Validate the length of the field
+    validateLength(this);
+
+    // Validate the email
+    if(this.type === 'email') {
+        validateEmail(this);
+    }
+}
+
+// Validates the length of the fields
+function validateLength(field) {
+    if(field.value.length > 0) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+}
+
+// Validate email (checks for @ in the value)
+function validateEmail(field) {
+    let emailText = field.value;
+    //check if the email text contains the @ sign
+    if (emailText.indexOf('@') !== -1) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
 }
